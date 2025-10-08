1afe687e1298a5c4b55c4627349fe9edf2678a54
Sati <sharaswati.Sahu@manektech.com>
2025-10-08 20:56:40 +0530

    feature A and B Completed

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
-
+THis is change 2
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
@@ -16,3 +16,6 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+
+This is change one
